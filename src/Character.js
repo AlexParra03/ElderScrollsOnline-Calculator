405290f42e28a0a9bf555234b2b2660a73a999cc
AlexParra03 <alex.user03@gmail.com>
2017-07-13 16:11:07 -0400

    added base files

---
+++ src/Character.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export class Character extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      class: "none",
+      weapon: [],
+      armor: [],
+      jewelry: [],
+      attributes: {}
+    }
+  }
+
+  render(){
+
+  }
+}
